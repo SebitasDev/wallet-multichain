5e617f48779a5a2c18df8d03925e357c195df1e6
sebastianramirezgarcua <camicarva119@gmail.com>
2025-12-05 16:28:20 -0300

    latest

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import dynamic from "next/dynamic";
-import {EmbeddedProvider} from "@/app/dashboard/hooks/embebed";
-
-const XOContractsProvider = dynamic(
-    () =>
-        import("@/app/dashboard/hooks/useGeneralWallet").then(
-            (m) => m.XOContractsProvider
-        ),
-    { ssr: false }
-);
+import { EmbeddedProvider } from "@/app/dashboard/hooks/embebed";
+import { XOContractsProvider } from "@/app/dashboard/hooks/useGeneralWallet";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    return <EmbeddedProvider>
-        <XOContractsProvider>
-            {children}
-        </XOContractsProvider>
-    </EmbeddedProvider>;
+    return (
+        <EmbeddedProvider>
+            <XOContractsProvider>
+                {children}
+            </XOContractsProvider>
+        </EmbeddedProvider>
+    );
 }
